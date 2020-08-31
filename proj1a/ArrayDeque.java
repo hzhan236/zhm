df92bab4572b92bb40947ab936c86a13442bb91c
hzhan236 <hzhan236@gmail.com>
2020-08-31 15:30:49 -0700

    modified ad rFactor
    quit

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,7 +3,7 @@ public class ArrayDeque<T> {
     private int size;
     private int nextFirst;
     private int nextLast;
-    private final int rFactor = 4;
+    private final int rFactor = 3;
 
     /**
      * Constructor for ArrayDeque()
